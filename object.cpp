52cff9953494ecd0d0c6af9704c0d447f5d95116
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-03-17 22:49:40 -0700

    Add files via upload

---
+++ object.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class sajib{
+public:
+int n;
+string s;
+};
+
+int main(){
+sajib myobj;
+myobj.n=15; 
+myobj.s="abcde";
+
+cout<<myobj.n<<endl;
+cout<<myobj.s<<endl;
+
+
+ return 0;
+}
